250148a2577b35bb0f82c2cf52c04eaf4f5321f6
anouk <konstantinova.anouk@gmail.com>
2019-05-24 00:50:03 +0300

    Generators commit

--- src/js/generators.js
+++ src/js/generators.js
@@ -7,17 +7,23 @@
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
   // TODO: write logic here
-  var randIndex = Math.floor(Math.random() * allowedTypes.size);
-  var randLevel = Math.floor(Math.random() * maxLevel);
-  yield new allowedTypes[randIndex](randLevel);
+  const randIndex = Math.floor(Math.random() * allowedTypes.length);
+  const randClass = allowedTypes[randIndex];
+  const randLevel = Math.floor(Math.random() * maxLevel) + 1;
+  const CharacterClass = dynamicClass(randClass);
+  yield new CharacterClass(randLevel);
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
   const team = new Array();
-  const generator = characterGenerator(allowedTypes, maxLevel);
-  for (let i = 0; i < characterCount.length; i += 1) {
+  for (let i = 0; i < characterCount; i += 1) {
+    const generator = characterGenerator(allowedTypes, maxLevel);
     team.push(generator.next().value);
   }
   return team;
+}
+
+export function dynamicClass(characterClass) {
+  return characterClass;
 }
--- test/generators.test.js
+++ test/generators.test.js
@@ -1,14 +1,24 @@
 import {characterGenerator} from '../src/js/generators';
 import {generateTeam} from '../src/js/generators';
 import Bowman from '../src/js/Bowman';
-import Swordsman from '../src/js/Swordsman';
 
-test('test characterGenerator', () => {
-    const allowedTypes = new Set([Bowman]);
+test('test characterGenerator Bowman(1)', () => {
+    const allowedTypes = [Bowman];
     const maxLevel = 1;
     const generator = characterGenerator(allowedTypes, maxLevel);
     const character = generator.next().value;
     const bowman = new Bowman(1);
     expect(character).toEqual(bowman);
+  });
+
+
+  test('test generateTeam Two Bowman(1)', () => {
+    const allowedTypes = [Bowman];
+    const maxLevel = 1;
+    const team = generateTeam(allowedTypes, maxLevel, 2);
+    const bowman = new Bowman(1);
+    expect(team.length).toBe(2);
+    expect(team[0]).toEqual(bowman);
+    expect(team[1]).toEqual(bowman);
   });
 
